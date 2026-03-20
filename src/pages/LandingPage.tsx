c0bac39b6e4ec25c288b63dccfaed1c2405291e7
adamandiayecisse0-debug <adamandiayecisse0@gmail.com>
2026-03-20 21:48:50 +0000

    feat: ajout VehiculesSection avec onglets Location/Achat et corection MarqueeStrip

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,14 +1,16 @@
 import Navbar from "../components/landing/Navbar";
 import Hero from "../components/landing/Hero";
-import MarqueeStrip from "../components/MarqueeStrip";
-
+import MarqueeStrip from "../components/landing/MarqueeStrip";
+import VehiculesSection from "../components/landing/VehiculesSection";
+  
 
 export default function LandingPage() {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-white">
       <Navbar />
       <Hero />
       <MarqueeStrip/>
+      <VehiculesSection/>
     </div>
   );
 }
